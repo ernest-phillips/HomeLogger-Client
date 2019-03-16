353ab928fa3b9eb2b0e3dd9188f8fe764808da0c
Ernest Phillips <ernestlphillips@gmail.com>
2019-03-16 09:51:11 -0700

    initial commit

---
+++ src/components/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <article>
+      <p><span className="first-word">Welcome</span> to the Community Story app!
+          This is an open-ended story that YOU create!
+          Collaborate with the world in developing this awesome 
+          fictional tale. 
+          Each portion of the story is only 140 characters long.
+          So be creative with how you add to the story. You can 
+          add as many posts as you like. 
+          Your posts can also be get Likes from visitors!
+          Have fun!
+      </p>
+    </article>
+  )
+}
