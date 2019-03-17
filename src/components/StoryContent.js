067522a3382128456b2a7d52b56f3619d24b659a
Ernest Phillips <ernestlphillips@gmail.com>
2019-03-16 17:32:05 -0700

    content added

--- src/components/StoryContent.js
+++ src/components/StoryContent.js
@@ -4,6 +4,11 @@ export default  function StoryContent (props) {
   return (
     <div>
       <p className="paragraph">{props.item.content}</p>
+    <div className="like-sect">
+        <img src="/public/images/heart.png" alt="heart"></img>
+        <p className="like-text">Likes</p>
+        <p>{props.item.author}</p>
+    </div>
     </div>
   )
 }
