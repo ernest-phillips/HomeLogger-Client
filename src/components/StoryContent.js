c91ded0991bc4d64a0e08f74ae57970aad33a126
Ernest Phillips <ernestlphillips@gmail.com>
2019-03-16 17:37:24 -0700

    updates

--- src/components/StoryContent.js
+++ src/components/StoryContent.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 export default  function StoryContent (props) {
   return (
-    <div>
+    <div className="post-add">
       <p className="paragraph">{props.item.content}</p>
     <div className="like-sect">
-        <img src="/public/images/heart.png" alt="heart"></img>
+        <img src="../../public/images/heart.png" alt="heart"></img>
         <p className="like-text">Likes</p>
         <p>{props.item.author}</p>
     </div>
