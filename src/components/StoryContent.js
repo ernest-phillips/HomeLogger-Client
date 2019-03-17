d7649aaf4aebc9708e9fe9489dc2cb864cfefd8b
Ernest Phillips <ernestlphillips@gmail.com>
2019-03-17 11:05:48 -0700

    more styling

--- src/components/StoryContent.js
+++ src/components/StoryContent.js
@@ -5,7 +5,7 @@ export default  function StoryContent (props) {
     <div className="post-add">
       <p className="paragraph">{props.item.content}</p>
     <div className="like-sect">
-        <img src="../../public/images/heart.png" alt="heart"></img>
+        <img src="C:\Users\ernes\Desktop\Coding\Capstone\community-story\public\images\heart.png" alt="heart"></img>
         <p className="like-text">Likes</p>
         <p>{props.item.author}</p>
     </div>
