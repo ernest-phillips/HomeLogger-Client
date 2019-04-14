9eed87b236f7cc249a974a8f2010ee45257412d4
Ernest Phillips <ernestlphillips@gmail.com>
2019-04-13 17:21:26 -0700

    moved css

--- src/components/App.js
+++ src/components/App.js
@@ -1,42 +1,36 @@
-import React, { Component } from 'react';
-import '../css/App.css';
-import About from './About'
-import Header from './Header'
-import Footer from './Footer'
-import SignIn from './SignIn'
-import NavBar from './NavBar'
-import FullStory from './FullStory'
-import Register from './Register'
-import {
-  BrowserRouter as Router, 
-  Route,
-  Switch
-  
-} from 'react-router-dom'
+import React, { Component } from "react";
+import "./css/App.css";
+import "./css/index.css";
+import "./css/login.css";
+import About from "./About";
+import Header from "./Header";
+import Footer from "./Footer";
+import SignIn from "./SignIn";
+import NavBar from "./NavBar";
+import FullStory from "./FullStory";
+import Register from "./Register";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-function App () {    
-      return (
-        <Router>
-          <div className="Site" >
-            <NavBar />
-              <Header />
-              <main className="Site-content">
-                <div className="inner-content">
-                  <Switch>
-                    <Route exact path="/" component={About} />
-                    <Route path="/sign-in" component={SignIn}/>
-                    <Route path="/story" component={FullStory}/>
-                    <Route path="/register" component={Register}/>
-                  </Switch>                  
-                </div>        
-              </main>
-              <Footer />
+function App() {
+  return (
+    <Router>
+      <div className="Site">
+        <NavBar />
+        <Header />
+        <main className="Site-content">
+          <div className="inner-content">
+            <Switch>
+              <Route exact path="/" component={About} />
+              <Route path="/sign-in" component={SignIn} />
+              <Route path="/story" component={FullStory} />
+              <Route path="/register" component={Register} />
+            </Switch>
           </div>
-        </Router>
-    );
-    
-    
-  
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
