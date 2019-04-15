94dc93d7b1217b93fe1206c6cfc5376f2661e2b7
Ernest Phillips <ernestlphillips@gmail.com>
2019-04-14 19:23:10 -0700

    add dependencies

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
-
-
+import {Field, reduxForm, focus} from 'redux-form';
+import {registerUser} from '../actions/users';
+import {login} from '../actions/auth';
+import {required, nonEmpty, matches, length, isTrimmed} from '../validators';
+const passwordLength = length({min: 10, max: 72});
+const matchesPassword = matches('password');
 export default class Register extends Component {
   onSubmit(values) {
     const { username, password} = values;
