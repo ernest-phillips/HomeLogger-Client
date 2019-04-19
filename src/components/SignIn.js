ea2a0462c5bccaa454b91df8f739286c0e707722
Ernest Phillips <ernestlphillips@gmail.com>
2019-04-18 21:20:12 -0700

    registration

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,27 +1,63 @@
-import React, { Component } from 'react'
-// import './css/login.css'
-// import '../index.css'
+import React, { Component } from "react";
+import { Field, reduxForm, focus } from "redux-form";
+import Input from "./Input";
+import { login } from "../actions/auth";
+import { required, nonEmpty } from "../validators";
 
-
-export default class SignIn extends Component {
+export class SignIn extends Component {
+  onSubmit(values) {
+    return this.props.dispatch(login(values.username, values.password));
+  }
   render() {
+    let error;
+    if (this.props.error) {
+      error = (
+        <div className="form-error" aria-live="polite">
+          {this.props.error}
+        </div>
+      );
+    }
     return (
-        <div class="flex-wrap">
+      <div class="flex-wrap">
         <fieldset>
-        <form id="login-form" action="" method="POST">
-            <input className="item" id="username-txt" type="text" placeholder="username" name="username" autocomplete="off" required/>
-            <input className="item" id="password-txt" type="password" placeholder="password" name="password" autocomplete="off" required />
+          <form
+            className="login-form"
+            onSubmit={this.props.handleSubmit(values => this.onSubmit(values))}
+          >
+            {error}
+            <Field
+              component={Input}
+              className="item"
+              name="username"
+              type="text"
+              placeholder="username"
+              validate={[required,nonEmpty]}
+            />
+            <Field
+              component={Input}
+              className="item"
+              name="password"
+              type="password"
+              placeholder="password"              
+              validate={[required,nonEmpty]}
+            />
             <button className="login-btn">Sign</button>
-        </form>
+          </form>
         </fieldset>
 
         <div className="test-account">
-            <p className="test-text">Test email: test@email.com</p>
-            
-            <p className="test-text">Password: password1</p>
-            <p className="reg-text">Not Registered? <a href="/register" >Sign Up</a></p>
+          <p className="test-text">Test email: test@email.com</p>
+
+          <p className="test-text">Password: password1</p>
+          <p className="reg-text">
+            Not Registered? <a href="/register">Sign Up</a>
+          </p>
         </div>
-    </div>
-    )
+      </div>
+    );
   }
 }
+export default reduxForm({
+  form: "login",
+  onSubmitFail: (errors, dispatch) => dispatch(focus("login", "username"))
+});
