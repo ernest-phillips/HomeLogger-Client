9b35cd300d6393548ea7e7b95fa31fbf1d24d62a
Ernest Phillips <ernestlphillips@gmail.com>
2019-05-04 13:46:50 -0700

    navbar state tests

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,36 @@
-import React from 'react'
+import React, { Component } from "react";
 
-export default function NavBar() {
-  return (
-    <div>
-      <nav>
-      <ul>
-        <li><a href="/">Home</a></li>        
-        <li><a href="/story">Full Story</a></li>
-        <li><a href="/sign-in">Sign In</a></li>
-      </ul>
-    </nav>
-    </div>
-  )
+class NavBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loggedIn: false
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({
+      loggedIn: !this.state.loggedIn
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <nav>
+        <ul>
+            <li> <a href="/">Home</a>   </li>
+            <li> <a href="/story">Full Story</a> </li>
+            <li> <a href="/sign-in">Sign In</a>  </li>
+          </ul>
+          {this.state.active }
+          <button type="button" onClick={this.handleClick}>Click</button>
+        </nav>
+      </div>
+    );
+  }
 }
+
+export default NavBar;
