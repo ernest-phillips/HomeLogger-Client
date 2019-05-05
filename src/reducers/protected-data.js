e7f0e1e3035fdc4409b4ba4bcbaff8bc3bea8b97
Ernest Phillips <ernestlphillips@gmail.com>
2019-05-05 15:48:55 -0700

    added logging to proted-data reducer

--- src/reducers/protected-data.js
+++ src/reducers/protected-data.js
@@ -10,12 +10,15 @@ const initialState = {
 };
 
 export default function reducer(state = initialState, action) {
+    
     if (action.type === FETCH_PROTECTED_DATA_SUCCESS) {
+        console.log("Fetched protected Success")
         return Object.assign({}, state, {
             data: action.data,
             error: null
         });
     } else if (action.type === FETCH_PROTECTED_DATA_ERROR) {
+        console.log("Fetched protected Failure")
         return Object.assign({}, state, {
             error: action.error
         });
