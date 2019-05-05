bf5e22acb15feb45f85d381a629047bd23ffb566
Ernest Phillips <ernestlphillips@gmail.com>
2019-05-05 15:10:30 -0700

    console.log token state

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -15,25 +15,30 @@ const initialState = {
 
 export default function reducer(state = initialState, action) {
     if (action.type === SET_AUTH_TOKEN) {
+        console.log("Setting Token")
         return Object.assign({}, state, {
             jwtToken: action.jwtToken
         });
     } else if (action.type === CLEAR_AUTH) {
+        console.log("Clearing Token")
         return Object.assign({}, state, {
             jwtToken: null,
             currentUser: null
         });
     } else if (action.type === AUTH_REQUEST) {
+        console.log("Requesting Token")
         return Object.assign({}, state, {
             loading: true,
             error: null
         });
     } else if (action.type === AUTH_SUCCESS) {
+        console.log("Success Token")
         return Object.assign({}, state, {
             loading: false,
             currentUser: action.currentUser
         });
     } else if (action.type === AUTH_ERROR) {
+        console.log("Authorization Error")
         return Object.assign({}, state, {
             loading: false,
             error: action.error
