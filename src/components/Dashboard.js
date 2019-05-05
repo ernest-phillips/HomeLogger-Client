0ec1829fb794f1b8ecccca7f52aae7d112e4f410
Ernest Phillips <ernestlphillips@gmail.com>
2019-05-05 15:11:31 -0700

    uncomment currentUser destructuring

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,40 +1,39 @@
-import React from 'react';
-import {connect} from 'react-redux';
-import requiresLogin from './RequiresLogin';
-import {fetchProtectedData} from '../actions/protected-data';
+import React from "react";
+import { connect } from "react-redux";
+import requiresLogin from "./RequiresLogin";
+import { fetchProtectedData } from "../actions/protected-data";
 // import Contribute from './Contribute'
-import NewPassageBox from './NewPassageBox'
-
+import NewPassageBox from "./NewPassageBox";
 
 export class Dashboard extends React.Component {
-    componentDidMount() {
-        this.props.dispatch(fetchProtectedData());
-    }
+  componentDidMount() {
+    this.props.dispatch(fetchProtectedData());
+  }
 
-    render() {
-        return (
-            <div className="dashboard">
-                <div className="dashboard-username">
-                    Username: {this.props.username}
-                </div>              
-                
-                <NewPassageBox />
+  render() {
+    return (
+      <div className="dashboard">
+        <div className="dashboard-username">
+          Username: {this.props.username}
+        </div>
 
-                <div className="dashboard-protected-data">
-                    Protected data: {this.props.protectedData}
-                </div>
-            </div>
-        );
-    }
+        <NewPassageBox />
+
+        <div className="dashboard-protected-data">
+          Protected data: {this.props.protectedData}
+        </div>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = state => {
-    // const {currentUser} = state.auth;
-    return {
-        username: state.auth.currentUser.username,
-       
-        protectedData: state.protectedData.data
-    };
+  const { currentUser } = state.auth;
+  return {
+    username: currentUser.username,
+
+    protectedData: state.protectedData.data
+  };
 };
 
 export default requiresLogin()(connect(mapStateToProps)(Dashboard));
