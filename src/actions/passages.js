88b1b4d91ca1390541241f57a98a58793f34b769
Ernest Phillips <ernestlphillips@gmail.com>
2019-05-06 05:12:28 -0700

    begin passages setup

---
+++ src/actions/passages.js
@@ -0,0 +1,26 @@
+import {SubmissionError} from 'redux-form';
+import {API_BASE_URL} from '../config';
+import {normalizeResponseErrors} from './utils';
+
+export const NewPassage = passage => dispatch => {
+    return fetch(`${API_BASE_URL}/passages`, {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(passage)
+    })
+        .then(res => normalizeResponseErrors(res))
+        .then(res => res.json())
+        .catch(err => {
+            const {reason, message, location} = err;
+            if (reason === 'ValidationError') {
+                // Convert ValidationErrors into SubmissionErrors for Redux Form
+                return Promise.reject(
+                    new SubmissionError({
+                        [location]: message
+                    })
+                );
+            }
+        });
+};
--- src/components/NewPassageBox.js
+++ src/components/NewPassageBox.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react'
+import {NewPassage} from '../actions/passages'
+import {Field, reduxForm, focus} from 'redux-form';
 
-export default class NewPassageBox extends Component {
+export class NewPassageBox extends Component {
+
+  onSubmit(passage){
+    const{author,body} = passage;
+    const newPassage = {author,body}
+    return this.props
+      .dispatch(NewPassage(newPassage))
+      
+  }
   render() {
     return (
       <div >
         <fieldset>
-        <form action="/passages" method="POST">
+        <form 
+          className="passage-box"
+          onSubmit={this.props.handleSubmit(passage =>
+              this.onSubmit(passage)
+            )}>
             <textarea 
             id="newPassage"
             className = "passage-entry" 
@@ -21,3 +35,8 @@ export default class NewPassageBox extends Component {
     )
   }
 }
+export default reduxForm({
+  form: 'newPassage',
+  onSubmitFail:(errors,dispatch) =>
+  dispatch(focus('newPassage', Object.keys(errors)[0]))
+})(NewPassageBox)
